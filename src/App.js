5bdc9de3c4331a8330d572d0f85567ad913a6efe
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-02-14 08:44:31 -0500

    Links to App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Outlet, Link } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Note-Taking App</h1>
+      <Link to="/signup"><button>Signup</button></Link>
+      <Link to="/login"><button>Login</button></Link>
+      <Link to="/logout"><button>Logout</button></Link>
+      <Outlet/>
     </div>
   );
 }
